dd26e7ca06bdfc4dec6f3ea49f2a4d0a8a62e7b8
jared-limjoco <limjocojared9@gmail.com>
2024-03-19 22:44:54 +0800

    Update annotationGet.js

--- src/pages/api/annotationGet.js
+++ src/pages/api/annotationGet.js
@@ -1,24 +1,15 @@
 import { connectToDatabase } from "@/util/mongodb";
 
 const handler = async (req, res) => {
- if (req.method === "POST") {
+  if (req.method === "POST") {
     const { db } = await connectToDatabase();
     const annotationTotalCount = req.body;
 
-    // First, find all imageIDs that are already annotated
-    const annotatedImageIDs = await db.collection("annotations").distinct("imageID");
-
-    // Then, find images that are not annotated yet
     const imgRecords = await db
       .collection("Image")
       .aggregate([
-        // Exclude images that are already annotated
-        { $match: { imageID: { $nin: annotatedImageIDs } } },
-        // Add a random field for sorting
         { $addFields: { rand: { $rand: {} } } },
-        // Sort by the random field
         { $sort: { rand: 1 } },
-        // Limit the results to annotationTotalCount
         { $limit: annotationTotalCount }
       ])
       .toArray();
@@ -26,7 +17,7 @@ const handler = async (req, res) => {
     res.json({
       imgRecords: imgRecords,
     });
- }
+  }
 };
 
 export default handler;
